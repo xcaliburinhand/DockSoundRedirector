504bc22539abc0bf7387f64171c38901fdb9c64d
xcaliburinhand <github@muteheadlight.net>
2012-08-23 19:53:59 -0400

    reset I535 device num

--- src/net/muteheadlight/docksoundredir/dockSoundRedirect.java
+++ src/net/muteheadlight/docksoundredir/dockSoundRedirect.java
@@ -189,9 +189,9 @@ public class dockSoundRedirect extends BroadcastReceiver{
     	int _deviceNum = 0x0000;
     	String dev = Build.DEVICE.toLowerCase();
     			
-    	if (Build.DISPLAY.toLowerCase().contains("cm")) {
+    	if (Build.DISPLAY.toLowerCase().contains("cm") || (dev.contains("i9000") && !Build.DISPLAY.contains("."))) {
 			_deviceNum = 0x800; //2048 handle legacy devices running CM
-		} else if (dev.contains("d2vzw") || dev.contains("d700") || dev.contains("g70") || dev.contains("gi1") || dev.contains("i400") || dev.contains("i500") || dev.contains("i510") || dev.contains("i896") || dev.contains("i897") || dev.contains("i9000") || dev.contains("i9003") || dev.contains("i997") || dev.contains("t959") || dev.contains("i997")) { //Galaxy S I
+		} else if (dev.contains("d700") || dev.contains("g70") || dev.contains("gi1") || dev.contains("i400") || dev.contains("i500") || dev.contains("i510") || dev.contains("i896") || dev.contains("i897") || dev.contains("i9000") || dev.contains("i9003") || dev.contains("i997") || dev.contains("t959") || dev.contains("i997")) { //Galaxy S I
     		_deviceNum = 0x1000; //4096
     	} else if (dev.contains("i9001")) {
     		_deviceNum = 0x4000; //16384
