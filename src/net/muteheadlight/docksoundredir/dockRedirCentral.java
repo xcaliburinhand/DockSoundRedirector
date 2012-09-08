c6a01068ecda4448040c4539de3ffe926e8a24c4
xcaliburinhand <github@muteheadlight.net>
2012-09-08 18:26:53 -0400

    updated German translations

--- src/net/muteheadlight/docksoundredir/dockRedirCentral.java
+++ src/net/muteheadlight/docksoundredir/dockRedirCentral.java
@@ -150,11 +150,11 @@ public final class dockRedirCentral {
     	int _deviceNum = 0x0000;
     	String dev = Build.DEVICE.toLowerCase();
     			
-    	if (Build.DISPLAY.toLowerCase().contains("cm") || (dev.contains("i9000") && !Build.DISPLAY.contains("."))) {
+    	if (Build.DISPLAY.toLowerCase().contains("cm") || (dev.contains("i9000") && (!Build.DISPLAY.contains(".") || Build.DISPLAY.matches("\\d\\.\\d")))) {
 			_deviceNum = 0x800; //2048 handle legacy devices running CM
 		} else if (dev.contains("d700") || dev.contains("g70") || dev.contains("gi1") || dev.contains("i400") || dev.contains("i500") || dev.contains("i510") || dev.contains("i727") || dev.contains("i777") || dev.contains("i896") || dev.contains("i897") || dev.contains("i9000") || dev.contains("i9003") || dev.contains("i997") || dev.contains("t959") || dev.contains("t989")) { //Galaxy S I
     		_deviceNum = 0x1000; //4096
-    	} else if (dev.contains("i9001")) {
+    	} else if (dev.contains("i9001") || dev.contains("s5360") || dev.contains("s5830")) {
     		_deviceNum = 0x4000; //16384
     	} else { //default
     		_deviceNum = 0x800; //2048
